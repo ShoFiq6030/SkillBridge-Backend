dd0c260fdbd8402d8bc66ea69019c7ecd010cef5
shofiq6030 <mdshofiq6030@gmail.com>
2026-03-01 00:47:32 +0600

    add better auth and signup api

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,24 @@
 import express, { Application } from "express";
 
-import cors from 'cors';
-
+import cors from "cors";
+import { auth } from "./lib/auth";
+import { toNodeHandler } from "better-auth/node";
 
 const app: Application = express();
 
-app.use(cors({
-    origin: process.env.APP_URL || "http://localhost:4000", // client side url
-    credentials: true
-}))
+app.use(
+  cors({
+    origin: process.env.APP_URL || "http://localhost:5000", // client side url
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 
-
 app.get("/", (req, res) => {
-    res.send("Welcome to SkillBridge API!");
+  res.send("Welcome to SkillBridge API!");
 });
 
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 export default app;
---
+++ src/lib/auth.ts
@@ -0,0 +1,73 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./prisma";
+import nodemailer from "nodemailer";
+import { verifyEmailTemplate } from "./email-template";
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // Use true for port 465, false for port 587
+  auth: {
+    user: process.env.APP_USER,
+    pass: process.env.APP_PASS,
+  },
+});
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql", // or "mysql", "postgresql", ...etc
+  }),
+  trustedOrigins: [process.env.APP_URL!],
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        defaultValue: "USER",
+        required: false,
+      },
+      status: {
+        type: "string",
+        defaultValue: "ACTIVE",
+        required: false,
+      },
+    },
+  },
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: false,
+    requireEmailVerification: true,
+  },
+  emailVerification: {
+    sendOnSignUp: true,
+    autoSignInAfterVerification: true,
+    sendVerificationEmail: async ({ user, url, token }, request) => {
+      try {
+        const verificationUrl = `${process.env.APP_URL}/verify-email?token=${token}`;
+        const info = await transporter.sendMail({
+          from: '"SkillBridge" <skillbridge@example.com>',
+          to: user.email,
+          subject: "Please verify your email!",
+          html: await verifyEmailTemplate(user.name, verificationUrl, url),
+        });
+        console.log("Message sent:", info.messageId);
+      } catch (err) {
+        console.error(err);
+        throw err;
+      }
+    },
+  },
+
+  socialProviders: {
+    google: {
+      prompt: "select_account consent",
+      accessType: "offline",
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+});
+
+//
+// GOOGLE_CLIENT_ID
+// GOOGLE_CLIENT_SECRET
